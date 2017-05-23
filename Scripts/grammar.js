d531dc8be8449db838650b9dd27e8981de2250d7
Kocann <kocela.ann@gmail.com>
2017-05-23 13:14:18 +0200

    more types examples

---
+++ Scripts/grammar.js
@@ -0,0 +1,5 @@
+var x = 1;
+var firstName = "Anna";
+var lastName = "Kocela";
+var thisIsAlsoBool = true;
+var isBool = true;
---
+++ Scripts/grammar.ts
@@ -0,0 +1,5 @@
+var x = 1;
+var firstName = "Anna";
+var lastName: string = "Kocela";
+var thisIsAlsoBool : boolean = true;
+var isBool = true;
